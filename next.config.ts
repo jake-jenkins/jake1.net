436643cf1ca2dbb39aa2d4012920990d9e9970c2
Jake Jenkins <jake@jake1.net>
2024-11-13 21:29:17 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
   output: "export",
-  basePath: "portfolio"
+  basePath: "/portfolio"
 };
 
 export default nextConfig;
