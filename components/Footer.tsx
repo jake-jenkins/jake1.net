4c3f6faf5d575eebae2db1bcac68e01f9e5faaf3
Jake Jenkins <jake@jake1.net>
2024-11-14 18:55:59 +0000

    change colours

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-lime-300 h-40 flex items-center justify-center px-4 text-center">
+    <footer className="border-lime-300 border-t-4 bg-slate-900 text-white h-40 flex items-center justify-center px-4 text-center">
       <p>Jake1.net © Jake Jenkins 2024.</p>
     </footer>
   );
