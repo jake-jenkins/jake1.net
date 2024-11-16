b7f7ad1b8029b71930301ca15960c76bc6b82862
Jake Jenkins <jake@jake1.net>
2024-11-16 16:51:55 +0000

    move to vercel

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
-  output: "export",
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
