07174f87f01bdad4307071121487982c7c0019ce
Jake Jenkins <jake@jake1.net>
2024-11-16 16:58:53 +0000

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const siteName = "Jake.ac";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <main className="flex-1 mt-14 lg:mt-16">{children}</main>
           <Footer />
         </div>
+        <Analytics />
       </body>
     </html>
   );
