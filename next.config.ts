9ddd8825be95fd36603383c74bb1a0e63d21ff41
Jake Jenkins <jake@jake1.net>
2024-11-17 16:06:38 +0000

    add headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,43 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(); geolocation=(); microphone=()'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://jake.ac'
+          },
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'same-site'
+          }
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
