fbdf9fc94bebd87e34f46f1cf5ccd56ef354b699
Jake Jenkins <jake@jake1.net>
2024-11-23 15:50:54 +0000

    add project content type

--- components/index.js
+++ components/index.js
@@ -2,10 +2,12 @@ import Page from "./Page";
 import Post from "./Post"
 import Heading from "./Heading"
 import Grid from "./Grid"
+import Project from "./Project";
 
 export const COMPONENTS = {
   page: Page,
   post: Post,
   heading: Heading,
-  grid: Grid
+  grid: Grid,
+  project: Project
 };
