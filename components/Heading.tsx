344e3d581f0102e3ef69a095b37b5a3e0ff02653
Jake Jenkins <jake@jake1.net>
2024-11-23 15:56:30 +0000

    update heading style

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,9 +1,21 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 
-export default function Heading({ blok }: {blok: any}) {
-  return (
-    <h2 className="text-8xl text-red-700" {...storyblokEditable(blok)}>
+export default function Heading({ blok }: { blok: any }) {
+  if (blok.level[0] === "1") {
+    <h1
+      className="text-indigo-800 text-3xl lg:text-5xl mt-2 mb-2 font-extrabold"
+      {...storyblokEditable(blok)}
+    >
       {blok.title}
-    </h2>
-  );
+    </h1>;
+  } else {
+    return (
+      <h2
+        className="text-indigo-800 text-2xl lg:text-3xl mb-2 font-bold"
+        {...storyblokEditable(blok)}
+      >
+        {blok.title}
+      </h2>
+    );
+  }
 }
