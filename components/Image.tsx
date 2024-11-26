ce63a225a7f37294fb33271e83cf0d8743afec43
Jake Jenkins <jake@jake1.net>
2024-11-26 23:08:08 +0000

    image update

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,12 +3,15 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 
 export default function Paragraph({ blok }: { blok: any }) {
   return (
-    <Image
-      src={blok.image.filename}
-      width={400}
-      height={200}
-      alt="image"
-      {...storyblokEditable(blok)}
-    />
+    <div className="flex justify-center">
+      <Image
+        className="mb-8"
+        src={blok.image.filename}
+        width={800}
+        height={400}
+        alt="image"
+        {...storyblokEditable(blok)}
+      />
+    </div>
   );
 }
